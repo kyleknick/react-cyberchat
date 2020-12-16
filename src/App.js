f6fd896b4dde578f13ed782d923d9458e1680cab
Kyle Knickerbocker <knickerbocker.chris@gmail.com>
2020-12-16 17:33:11 -0600

    update css styling

--- src/App.js
+++ src/App.js
@@ -49,4 +49,57 @@ function SignOut() {
   )
 }
 
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef. orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const[formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+    
+    setFormValue('')
+  }
+
+  return (
+    <div>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+    </div>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue}onChange={(e) => setFormValue(e.target.value)} />
+
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  ) 
+}
+
+
 export default App;
