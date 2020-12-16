f35704f7eaaa05572c9a651fd5bf24af62110441
Kyle Knickerbocker <knickerbocker.chris@gmail.com>
2020-12-16 16:35:49 -0600

    add firebase initialize data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase.auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA-tcG_vmpuwDQ7AWBRtyPQ8zzoCWZMguo",
+  authDomain: "spinitchat.firebaseapp.com",
+  projectId: "spinitchat",
+  storageBucket: "spinitchat.appspot.com",
+  messagingSenderId: "917377122174",
+  appId: "1:917377122174:web:d1cf858605026272667d2f",
+  measurementId: "G-T21T8K43E2"
+})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
