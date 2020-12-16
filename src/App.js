00ff56d61d408fd46cf572e27900f4d225c8e335
Kyle Knickerbocker <knickerbocker.chris@gmail.com>
2020-12-16 17:10:08 -0600

    add google auth and sigin signout

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import 'firebase.auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+const [user] = useAuthState(auth); 
+
 firebase.initializeApp({
   apiKey: "AIzaSyA-tcG_vmpuwDQ7AWBRtyPQ8zzoCWZMguo",
   authDomain: "spinitchat.firebaseapp.com",
@@ -24,8 +26,27 @@ function App() {
       <header className="App-header">
         
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
 
 export default App;
